db29579e303c2a46ff588ee30e4265d7c0b37590
masatomo-sugawara <masatomo.s1982@gmail.com>
2022-07-22 09:54:22 +0900

    Organismの作成1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>TEST</PrimaryButton>
-      <br />
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard />
     </div>
   );
 }
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>test@test.com</dd>
+        <dt>TEL</dt>
+        <dd>099-123-4568</dd>
+        <dt>会社名</dt>
+        <dd>あいうえお株式会社</dd>
+        <dt>WEB</dt>
+        <dd>test.com</dd>
+      </dl>
+    </div>
+  );
+};
